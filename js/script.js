36865d8d8498644ae3d022f5a18b16eeb7ca44e3
MUNNA KUMAR <96313339+MunnaKumar27@users.noreply.github.com>
2021-12-30 18:54:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Competitive Programmer","Web Developer","Navodayan","Nitian"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
